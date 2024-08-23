16351530cf8d3e7cd145b921e94a09fee65921db
Alex Luchian <alexandru.luchian@gmail.com>
2024-08-23 15:08:44 -0400

    Remove excessive logging from the exception class.

--- src/main/java/com/alex/aem/exception/RomanNumeralConversionException.java
+++ src/main/java/com/alex/aem/exception/RomanNumeralConversionException.java
@@ -1,22 +1,17 @@
 package com.alex.aem.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class RomanNumeralConversionException extends RuntimeException {
-    private static final Logger log = LoggerFactory.getLogger(RomanNumeralConversionException.class);
     private final String errorCode;
 
     /**
      * Constructs a new RomanNumeralConversionException with the specified detail message and error code.
      *
-     * @param message the detail message
+     * @param message   the detail message
      * @param errorCode the error code
      */
     public RomanNumeralConversionException(final String message, final String errorCode) {
         super(message);
         this.errorCode = errorCode;
-        log.error("RomanNumeralConversionException created: {} (Error code: {})", message, errorCode);
     }
 
     /**
